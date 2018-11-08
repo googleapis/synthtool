349292a268fe1eaf1691275f43e56b7e0fef72ea
Thea Flowers <theaflowers@google.com>
2018-11-08 12:40:23 -0800

    Release 2018.11.08 (#123)

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import setuptools
 
 name = 'gcp-synthtool'
 description = ''
-version = '0.10.0'
+version = '2018.11.08'
 release_status = 'Development Status :: 3 - Alpha'
 dependencies = [
     "click",
