aa6de4a6fb42a6c97f5a89c1b4373ee653b22fe1
Thea Flowers <theaflowers@google.com>
2018-11-14 23:21:00 -0800

    Add metadata reporting for Git sources.

---
+++ synthtool/metadata.py
@@ -0,0 +1,59 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import atexit
+import functools
+import sys
+
+import google.protobuf.json_format
+
+from synthtool import log
+from synthtool.protos import metadata_pb2
+
+
+_metadata = metadata_pb2.Metadata()
+
+
+def reset() -> None:
+    """Clear all metadata so far."""
+    global _metadata
+    _metadata = metadata_pb2.Metadata()
+
+
+def get():
+    return _metadata
+
+
+def add_git_source(**kwargs) -> None:
+    """Adds a git source to the current metadata."""
+    _metadata.sources.add(git=metadata_pb2.GitSource(**kwargs))
+
+
+def write(outfile: str = "synth.metadata") -> None:
+    """Writes out the metadata to a file."""
+    jsonified = google.protobuf.json_format.MessageToJson(_metadata)
+
+    with open(outfile, "w") as fh:
+        fh.write(jsonified)
+
+    log.debug(f"Wrote metadata to {outfile}.")
+
+
+def register_exit_hook(**kwargs) -> None:
+    atexit.register(functools.partial(write, **kwargs))
+
+
+# Only register this hook if pytest is not active.
+if "pytest" not in sys.modules:  # pragma: no cover
+    register_exit_hook()
---
+++ tests/test_metadata.py
@@ -0,0 +1,47 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+
+from synthtool import metadata
+
+
+def test_add_git_source():
+    metadata.reset()
+
+    metadata.add_git_source(sha="sha", name="name", remote="remote")
+
+    current = metadata.get()
+
+    assert current.sources[0].git.sha == "sha"
+    assert current.sources[0].git.name == "name"
+    assert current.sources[0].git.remote == "remote"
+
+
+def test_write(tmpdir):
+    metadata.reset()
+
+    metadata.add_git_source(sha="sha", name="name", remote="remote")
+
+    output_file = tmpdir / "synth.metadata"
+
+    metadata.write(str(output_file))
+
+    data = output_file.read()
+
+    # Ensure the file was written, that *some* metadata is in it, and that it
+    # is valid JSON.
+    assert data
+    assert "sha" in data
+    assert json.loads(data)
