2842dc46d335353a1008219eb04d779b86863981
Jeff Ching <chingor@google.com>
2018-11-20 14:47:51 -0800

    Add ability to format java code with a custom google-java-format version

---
+++ synthtool/languages/java.py
@@ -0,0 +1,48 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from synthtool import cache
+from synthtool import log
+from synthtool import shell
+from pathlib import Path
+from typing import List
+
+def format_code(path: str, version: str = "1.6") -> None:
+    """
+    Runs the google-java-format jar against all .java files found within the
+    provided path.
+    """
+    jar_name = f"google-java-format-{version}.jar"
+    jar = cache.get_cache_dir() / jar_name
+    if not jar.exists():
+        _download_formatter(version, jar)
+
+    # Find all .java files in path and run the formatter on them
+    files = _find_files(path)
+
+    # Run the formatter as a jar file
+    log.info("Running java formatter on {} files".format(len(files)))
+    shell.run(["java", "-jar", jar, "--replace"] + files)
+
+def _download_formatter(version: str, dest: Path) -> None:
+    url = f"https://github.com/google/google-java-format/releases/download/google-java-format-{version}/google-java-format-{version}-all-deps.jar"
+    log.info("Downloading java formatter")
+    shell.run(["curl", "-L", "-o", dest, url], hide_output=False)
+
+def _find_files(basedir: str, suffix: str = ".java") -> List[str]:
+    return [os.path.join(root, name)
+            for root, _dirs, files in os.walk(basedir)
+            for name in files
+            if name.endswith(suffix)]
