1c63bc84b3ebdc3c585e1c21493fd77935febda4
Chris Wilcox <crwilcox@google.com>
2018-11-29 14:11:47 -0800

    Release 2018.11.29.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import setuptools
 
 name = 'gcp-synthtool'
 description = ''
-version = '2018.11.29'
+version = '2018.11.29.2'
 release_status = 'Development Status :: 3 - Alpha'
 dependencies = [
     "click",
