f7098601858f14f6fd840bb6345664990cb746c6
Jeffrey Rennie <rennie@google.com>
2019-12-09 14:38:49 -0800

    Remove unnecessary log.debug line because
    the log file has been closed by the time tmp.cleanup()
    tries to write a log statement.

--- synthtool/tmp.py
+++ synthtool/tmp.py
@@ -33,7 +33,6 @@ def tmpdir() -> Path:
 def cleanup():
     for path in _tempdirs:
         shutil.rmtree(str(path))
-    log.debug(f"Cleaned up {len(_tempdirs)} temporary directories.")
 
 
 atexit.register(cleanup)
