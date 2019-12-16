afd0fea6433d56ed7ef5fcb49a9cd1fe68663b2d
Jeffrey Rennie <rennie@google.com>
2019-12-16 15:02:31 -0800

    feat: remove obsolete files during synthesis again (#345)
    * feat: remove obsolete files during synthesis
    
    This reverts commit a2e4f454d56cc378d8c5d582db2e4d7da6845407.
    
    * Cope with bad links.
    
    And add a test.
    
    * Lint
    
    * Skip test on Windows because symlinks require Admin privileges on Windows.

--- synthtool/__main__.py
+++ synthtool/__main__.py
@@ -22,6 +22,7 @@ import pkg_resources
 
 import synthtool.log
 import synthtool.metadata
+import time
 
 
 try:
@@ -76,8 +77,6 @@ def main(synthfile: str, metadata: str, extra_args: Sequence[str]):
     """
     _extra_args.extend(extra_args)
 
-    synthtool.metadata.register_exit_hook(outfile=metadata)
-
     synth_file = os.path.abspath(synthfile)
 
     if os.path.lexists(synth_file):
@@ -89,7 +88,16 @@ def main(synthfile: str, metadata: str, extra_args: Sequence[str]):
         if spec.loader is None:
             raise ImportError("Could not import synth.py")
 
-        spec.loader.exec_module(synth_module)  # type: ignore
+        start_time = time.time() - 1  # -1 fudge factor to make sure we don't
+        # miss files that were quickly created.
+        old_metadata = synthtool.metadata.read_or_empty(metadata)
+
+        try:
+            spec.loader.exec_module(synth_module)  # type: ignore
+        finally:
+            synthtool.metadata.add_new_files(start_time)
+            synthtool.metadata.remove_obsolete_files(old_metadata)
+            synthtool.metadata.write(metadata)
 
     else:
         synthtool.log.exception(f"{synth_file} not found.")
