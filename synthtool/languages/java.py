d388724427279161119a5fd2f9f7909e9697ad10
Jeff Ching <chingor@google.com>
2020-01-22 14:04:48 -0800

    fix: hardcode 2020 as the license year

--- synthtool/languages/java.py
+++ synthtool/languages/java.py
@@ -29,7 +29,7 @@ JAR_DOWNLOAD_URL = "https://github.com/google/google-java-format/releases/downlo
 DEFAULT_FORMAT_VERSION = "1.7"
 GOOD_LICENSE = f"""
 /*
- * Copyright {date.today().year} Google LLC
+ * Copyright 2020 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
