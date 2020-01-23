29cc1fe9ca05c192ee530c073f61892e0c216f2e
Jeff Ching <chingor@google.com>
2020-01-23 10:02:52 -0800

    revert: test_template import changes

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -17,7 +17,7 @@ import stat
 import sys
 from pathlib import Path
 
-from synthtool import gcp
+from synthtool.gcp import common
 from synthtool.sources import templates
 
 
@@ -72,7 +72,7 @@ def test_load_samples():
     cwd = os.getcwd()
     os.chdir(FIXTURES)
 
-    common_templates = gcp.CommonTemplates()
+    common_templates = common.CommonTemplates()
     metadata = {}
     common_templates._load_samples(metadata)
     # should have loaded samples.
@@ -118,7 +118,7 @@ def test_readme_partials():
     cwd = os.getcwd()
     os.chdir(FIXTURES)
 
-    common_templates = gcp.CommonTemplates()
+    common_templates = common.CommonTemplates()
     metadata = {}
     common_templates._load_partials(metadata)
     # should have populated introduction from partial.
