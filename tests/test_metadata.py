1bd27de10555dd4aea88c96ae603dea37b455286
Jeffrey Rennie <rennie@google.com>
2020-02-06 10:36:01 -0800

    Fix broken test.

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -198,7 +198,7 @@ def test_append_git_log_to_metadata(source_tree):
     # Match 2 log lines.
     assert re.match(
         r"[0-9A-Fa-f]+\ncode/c\n+[0-9A-Fa-f]+\ncode/b\n+",
-        mdata.sources[0].git.log,
+        mdata.sources[2].git.log,
         re.MULTILINE,
     )
     # Make sure the local path field is not recorded.
