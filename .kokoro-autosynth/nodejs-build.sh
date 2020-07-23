86418fb2eb708ba0670e4450dc1e1796a1816f47
Justin Beckwith <justin.beckwith@gmail.com>
2020-07-23 09:42:40 -0700

    pr feedback

--- .kokoro-autosynth/nodejs-build.sh
+++ .kokoro-autosynth/nodejs-build.sh
@@ -17,9 +17,16 @@ set -eo pipefail
 
 cd ${KOKORO_ARTIFACTS_DIR}/github/synthtool
 
+# Ensure `curl` is available
+sudo apt-get update && sudo apt-get -y install curl
+
 # Use `nvm` to bootstrap the installation of node.js and npm.
 # To learn more: https://github.com/nvm-sh/nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+# Activate `nvm` so the binary is available on the path
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 # Use the latest LTS version of nodejs.  This can change over time, but
 # should prevent the need to modify this file every year.
