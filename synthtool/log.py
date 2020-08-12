4eae0ed992c81291471f1482ce7a3629067a3d5d
Jeffrey Rennie <rennie@google.com>
2020-08-11 18:57:16 -0700

    fix: really turn off verbose logging from watchdog
    https://github.com/googleapis/synthtool/pull/721 failed to resolve the issue.
    It turned dialed down logging for everything except watchdog.
    I've tested this and confirmed this new code will work.

--- synthtool/log.py
+++ synthtool/log.py
@@ -40,7 +40,7 @@ def _setup_logging(color: bool = bool(ColoredFormatter)):
     logging.setLoggerClass(LoggerWithSuccess)
 
     # Silence any noisy loggers here.
-
+    logging.getLogger("watchdog.observers").setLevel(logging.INFO)
 
 def configure_logger(name: str, color: bool = bool(ColoredFormatter)):
     """Create and configure the default logger for autosynth.
@@ -48,10 +48,10 @@ def configure_logger(name: str, color: bool = bool(ColoredFormatter)):
     log severity.
     """
     logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
 
     handler = logging.StreamHandler()
-    handler.setLevel(logging.INFO)
+    handler.setLevel(logging.DEBUG)
 
     if color is True and sys.stdout.isatty():
         formatter = ColoredFormatter(
