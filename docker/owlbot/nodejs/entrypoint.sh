8dacc982e859b79acfd456b296815c300fc25059
bcoe <bencoe@google.com>
2021-02-17 18:19:37 -0800

    build: source nvm

--- docker/owlbot/nodejs/entrypoint.sh
+++ docker/owlbot/nodejs/entrypoint.sh
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -x
+set -e
+source /root/.nvm/nvm.sh
 export PYTHONPATH="/synthtool"
+set -x
 python owlbot.py
