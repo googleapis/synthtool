55ab050c14d8ab7b9212f0ea67de3b870f61233f
Jeff Ching <chingor@google.com>
2021-03-04 17:04:05 -0800

    feat: add Java owlbot Docker image

---
+++ docker/owlbot/java/scripts/fix_license_headers.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+for file in `git diff-tree --no-commit-id -r $(git rev-parse HEAD) --name-only --diff-filter=M`
+do
+  # if the header year changed in the last diff,
+  # then restore the previous year
+  old_copyright=$(git show HEAD~1:${file} | head -n 10 | egrep -o -e "Copyright ([[:digit:]]{4})")
+  new_copyright=$(git show HEAD:${file} | head -n 10 | egrep -o -e "Copyright ([[:digit:]]{4})")
+  if [ ! -z "${old_copyright}" ] && [ ! -z "${new_copyright}" ] && [ "${old_copyright}" != "${new_copyright}" ]
+  then
+    echo "Restoring copyright in ${file} to '${old_copyright}'"
+    sed -i "s/${new_copyright}/${old_copyright}/1" ${file}
+  fi
+done
