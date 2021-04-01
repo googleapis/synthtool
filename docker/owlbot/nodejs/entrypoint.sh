c4df7cfc00edf3eff6c21c4c221558285c0f7048
Jeffrey Rennie <rennie@google.com>
2021-03-31 17:15:51 -0700

    fix: non-root users can run the nodejs docker post processor
    Fixes https://github.com/googleapis/repo-automation-bots/issues/1585 via 3 methods:
    1. Install node using apt-get instead of nvm.
    2. Pre-install all the dependencies of protobufjs.  It's sneaky and tries to install things when you run it.
    3. Make sure the /synthtool directory tree is readable by all.

--- docker/owlbot/nodejs/entrypoint.sh
+++ docker/owlbot/nodejs/entrypoint.sh
@@ -13,9 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
-source /root/.nvm/nvm.sh
-set -x
+set -ex
 
 if [ -f owlbot.py ]; then
     python owlbot.py
