3fc6b333bed8335c40bd81dd6e2151f4e6eab8dc
Bu Sun Kim <busunkim@google.com>
2021-04-29 20:29:25 +0000

    chore: fix nvm dir

--- .kokoro-autosynth/build.sh
+++ .kokoro-autosynth/build.sh
@@ -20,6 +20,7 @@ cd synthtool
 # Install NPM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm install node
 nvm install-latest-npm
 
