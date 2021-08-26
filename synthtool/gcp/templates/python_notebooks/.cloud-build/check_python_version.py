790c29821a7812191d1730aad8f11abbf42686c8
Lo Ferris <loferris@google.com>
2021-08-25 17:35:56 -0700

    updating .cloud-build to recent versions

--- synthtool/gcp/templates/python_notebooks/.cloud-build/check_python_version.py
+++ synthtool/gcp/templates/python_notebooks/.cloud-build/check_python_version.py
@@ -15,13 +15,13 @@
 import sys
 
 MINIMUM_MAJOR_VERSION = 3
-MINIMUM_MINOR_VERSION = 3
+MINIMUM_MINOR_VERSION = 5
 
 if (
     sys.version_info.major < MINIMUM_MAJOR_VERSION
     and sys.version_info.minor < MINIMUM_MINOR_VERSION
 ):
-    print("Error: Python version less than 3.5")
+    print(f"Error: Python version less than {MINIMUM_MAJOR_VERSION}.{MINIMUM_MINOR_VERSION}")
     exit(1)
 else:
     print(f"Python version acceptable: {sys.version}")
