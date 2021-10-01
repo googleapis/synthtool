756cbcffed06164edba2bb5ea8827527bf737b09
Bu Sun Kim <busunkim@google.com>
2021-10-01 16:32:56 +0000

    chore: blacken, move image upload to periodic

--- synthtool/gcp/common.py
+++ synthtool/gcp/common.py
@@ -236,7 +236,7 @@ class CommonTemplates:
         # Don't add samples templates if there are no samples
         if "samples" not in kwargs:
             self.excludes += ["samples/AUTHORING_GUIDE.md", "samples/CONTRIBUTING.md"]
-        
+
         # Assume the python-docs-samples Dockerfile is used for samples by default
         if "custom_samples_dockerfile" not in kwargs:
             kwargs["custom_samples_dockerfile"] = False
