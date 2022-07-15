42efa27fb79672019a5a4d6fb5cbefec41446844
Tomo Suzuki <suztomo@google.com>
2022-07-15 16:06:40 -0400

    build: debugging

--- synthtool/languages/java.py
+++ synthtool/languages/java.py
@@ -848,7 +848,7 @@ def owlbot_entrypoint(staging_dir: str = STAGING_DIR) -> None:
                     print("Calling " + str(owlbot_py)
                           + " with cwd : " + str(dest))
                     ret = subprocess.run(["python", owlbot_py], cwd=dest,
-                                         check=True)
+                                         check=True, capture_output=True)
 
                     print("Return code: " + str(ret.returncode))
                     print('Subprocess stdout:')
