5db4550bb0ac25f512b98dd55879e3fc8cd69f23
Sofia Leon <sofialeon@google.com>
2022-07-14 19:28:59 -0700

    run lint

--- synthtool/languages/node_mono_repo.py
+++ synthtool/languages/node_mono_repo.py
@@ -253,9 +253,11 @@ def walk_through_owlbot_dirs(dir: Path):
     A list of client libs
     """
     owlbot_dirs = []
-    packages_to_exclude = [r'gapic-node-templating']
+    packages_to_exclude = [r"gapic-node-templating"]
     for path_object in dir.glob("packages/**/.OwlBot.yaml"):
-        if path_object.is_file() and not re.search('(?:% s)' % '|'.join(packages_to_exclude), str(Path(path_object))):
+        if path_object.is_file() and not re.search(
+            "(?:% s)" % "|".join(packages_to_exclude), str(Path(path_object))
+        ):
             owlbot_dirs.append(str(Path(path_object).parents[0]))
 
     return owlbot_dirs
