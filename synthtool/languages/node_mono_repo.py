d3336a2b203337588b7f86862d1e5e8820595c99
Sofia Leon <sofialeon@google.com>
2022-08-09 21:42:07 -0700

    get riid of import

--- synthtool/languages/node_mono_repo.py
+++ synthtool/languages/node_mono_repo.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from copy import copy
 import json
 from jinja2 import FileSystemLoader, Environment
 from pathlib import Path
