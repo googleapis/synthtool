1b6809b6e262e0d9794293e7c867d11acfd3eded
Tomo Suzuki <suztomo@google.com>
2022-10-31 21:41:45 -0400

    monorepo variable comment

--- synthtool/languages/java.py
+++ synthtool/languages/java.py
@@ -485,6 +485,7 @@ def common_templates(
         **kwargs: Additional options for CommonTemplates.java_library()
     """
     kwargs["metadata"] = _common_template_metadata()
+    # MONOREPO environment variable is set in entrypoint.sh
     kwargs["monorepo"] = os.environ.get('MONOREPO') == 'true'
     templates = gcp.CommonTemplates(template_path=template_path).java_library(**kwargs)
 
