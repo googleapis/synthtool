7197a001ffb6d8ce7b0b9b11c280f0c536c1033a
Anthonios Partheniou <partheniou@google.com>
2023-01-05 15:56:31 -0500

    feat(python): add support for python 3.11 (#1742)
    * feat: add support for python 3.11
    
    * add kokoro configs for python 3.11

--- synthtool/gcp/common.py
+++ synthtool/gcp/common.py
@@ -265,7 +265,7 @@ class CommonTemplates:
         if "default_python_version" not in kwargs:
             kwargs["default_python_version"] = "3.8"
         if "unit_test_python_versions" not in kwargs:
-            kwargs["unit_test_python_versions"] = ["3.7", "3.8", "3.9", "3.10"]
+            kwargs["unit_test_python_versions"] = ["3.7", "3.8", "3.9", "3.10", "3.11"]
 
         if "system_test_python_versions" not in kwargs:
             kwargs["system_test_python_versions"] = ["3.8"]
