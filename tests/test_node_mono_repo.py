623e5dc60a01ff6ba6987908b6e58d0a41b86023
Sofia Leon <sofialeon@google.com>
2023-01-17 15:37:06 -0800

    fix(node-mono-repo): call owlbot.py when specified

--- tests/test_node_mono_repo.py
+++ tests/test_node_mono_repo.py
@@ -473,6 +473,25 @@ def test_entrypoint_args_with_specified_dirs():
     assert node_mono_repo.owlbot_main.called_with(dir="packages/google-cloud-compute")
 
 
+def test_entrypoint_with_owlbot_py():
+    with util.copied_fixtures_dir(FIXTURES / "nodejs_mono_repo_with_staging"):
+        node_mono_repo.owlbot_main = MagicMock()
+        node_mono_repo.owlbot_entrypoint(
+            specified_owlbot_dirs="packages/google-cloud-workflows-executions"
+        )
+        node_mono_repo.owlbot_main.assert_called_with(
+            relative_dir="packages/google-cloud-workflows-executions",
+            templates_excludes=["src/index.ts"],
+            staging_excludes=[
+                "src/v1/index.ts",
+                "src/v1beta/index.ts",
+                "src/index.ts",
+                "README.md",
+                "package.json",
+            ],
+        )
+
+
 def test_entrypoint_args_with_no_arg():
     node_mono_repo.walk_through_owlbot_dirs = MagicMock()
     node_mono_repo.owlbot_entrypoint()
