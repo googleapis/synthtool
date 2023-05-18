e26b3ce36a3bc8bdaa83adb290c942acfe18ef33
Tomo Suzuki <suztomo@google.com>
2023-05-18 13:33:02 -0400

    chore: use build Java 17 and run tests on Java 8

--- synthtool/gcp/templates/java_library/.kokoro/build.sh
+++ synthtool/gcp/templates/java_library/.kokoro/build.sh
@@ -47,7 +47,17 @@ set +e
 
 case ${JOB_TYPE} in
 test)
-    mvn test -B -ntp -Dclirr.skip=true -Denforcer.skip=true
+    # Maven surefire plugin (unit tests) allows us to specify JVM to run the tests.
+    # https://maven.apache.org/surefire/maven-surefire-plugin/test-mojo.html#jvm
+    # If we rely on certain things only available in newer JVM than Java 8, this
+    # tests detect the usage.
+    SUREFIRE_JVM_OPT=""
+    if [ -n "${JAVA8_HOME}" ]; then
+      SUREFIRE_JVM_OPT="-Djvm=${JAVA8_HOME}/bin/java"
+      echo "To use ${SUREFIRE_JVM_OPT} for unit tests runtime"
+    fi
+
+    mvn test -B -ntp -Dclirr.skip=true -Denforcer.skip=true ${SUREFIRE_JVM_OPT}
     RETURN_CODE=$?
     ;;
 lint)
