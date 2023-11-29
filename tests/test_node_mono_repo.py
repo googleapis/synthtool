62fecb39e47ddbd283181d141c2332c06ce2fb21
sofisl <55454395+sofisl@users.noreply.github.com>
2023-11-29 14:20:06 -0800

    fix(node-mono-repo): execute non-ESM compileProtos command when not ESM (#1906)
    * fix(node): ensure compileProtos is run normally if not esm

--- tests/test_node_mono_repo.py
+++ tests/test_node_mono_repo.py
@@ -372,6 +372,28 @@ class TestPostprocess(TestCase):
         )
 
     @patch("synthtool.shell.run")
+    def test_compile_protos_hermetic(self, shell_run_mock):
+        node_mono_repo.compile_protos_hermetic(relative_dir="any", is_esm=False)
+        calls = shell_run_mock.call_args_list
+        assert any(
+            [
+                "/node_modules/.bin/compileProtos src" in " ".join(call[0][0])
+                for call in calls
+            ]
+        )
+
+    @patch("synthtool.shell.run")
+    def test_compile_protos_hermetic_esm(self, shell_run_mock):
+        node_mono_repo.compile_protos_hermetic(relative_dir="any", is_esm=True)
+        calls = shell_run_mock.call_args_list
+        assert any(
+            [
+                "/node_modules/.bin/compileProtos esm/src --esm" in " ".join(call[0][0])
+                for call in calls
+            ]
+        )
+
+    @patch("synthtool.shell.run")
     def test_postprocess_gapic_library(self, shell_run_mock):
         node_mono_repo.postprocess_gapic_library()
         calls = shell_run_mock.call_args_list
