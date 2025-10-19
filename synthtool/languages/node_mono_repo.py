3ecfd9359ec9fdda59fa47a5fb5eff44b8b20f99
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-10-19 18:59:39 -0400

    spelling: that
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- synthtool/languages/node_mono_repo.py
+++ synthtool/languages/node_mono_repo.py
@@ -404,7 +404,7 @@ def walk_through_owlbot_dirs(dir: Path, search_for_changed_files: bool):
     if search_for_changed_files:
         try:
             # Need to run this step first in the post processor since we only clone
-            # the branch the PR is on in the Docker container
+            # the branch that the PR is on in the Docker container
             output = subprocess.run(
                 ["git", "fetch", "origin", "main:main", "--deepen=200"]
             )
